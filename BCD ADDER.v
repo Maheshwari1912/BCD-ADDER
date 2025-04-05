bc25910e8af8646be9da22a44fd3ab4ca4246fc7
Maheshwari1912 <mahiratthapuram@gmail.com>
2025-04-05 10:08:06 +0530

    Update BCD ADDER.v

--- BCD ADDER.v
+++ BCD ADDER.v
@@ -1,3 +1,4 @@
+
 module bcd_adder(
   input [3:0] A,
   input [3:0] B,
@@ -10,23 +11,7 @@ module bcd_adder(
   wire correction;
   assign binarysum = A + B + cin;
   assign correction = (binarysum > 9);
-  assign bcdsum = coorection ? (binarysum + 4'b0110) : binarysum;
-  assign sum = bcd_sum[3:0];
-  assign cout = (correction || binarysum[4]);
-endmodule
-module bcd_adder(
-  input [3:0] A,
-  input [3:0] B,
-  input cin,
-  output [3:0] sum,
-  output cout
-);
-  wire [4:0] binarysum;
-  wire [3:0] bcd_sum;
-  wire correction;
-  assign binarysum = A + B + cin;
-  assign correction = (binarysum > 9);
-  assign bcdsum = coorection ? (binarysum + 4'b0110) : binarysum;
+  assign bcd_sum = coorection ? (binarysum + 4'b0110) : binarysum;
   assign sum = bcd_sum[3:0];
   assign cout = (correction || binarysum[4]);
 endmodule
